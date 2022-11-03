4a9fc8d96a3e6b0963b0529967fd22be2ea32058
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-11-03 13:39:16 -0600

    Started to develop the appointment endpoint

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from dbcreds import production_mode
-import endpoints.user
+import endpoints.user, endpoints.user_login
 
 # calling the Flask function which will return a value that I will be used for my API
 app = Flask(__name__)
+
+##############################################################################
+# USER #
+##############################################################################
 
 @app.post('/api/user')
 def post_user():
@@ -16,6 +20,14 @@ def get_user():
 @app.patch('/api/user')
 def patch_user():
     return endpoints.user.patch()
+
+##############################################################################
+# USER-LOGIN #
+##############################################################################
+
+@app.post('/api/user-login')
+def log_in_user():
+    return endpoints.user_login.post()
 
 # if statement to check if the production_mode variable is true, if yes, run in production mode, if not, run in testing mode
 if (production_mode):
