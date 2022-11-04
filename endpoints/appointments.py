82b26c7010bcc007d86080c8f0fe5dbb8f2ed00b
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-11-03 20:46:24 -0600

    Made possible patch an appointment

--- endpoints/appointments.py
+++ endpoints/appointments.py
@@ -22,25 +22,47 @@ def post():
         return make_response(json.dumps("Sorry, an error has occurred", default=str), 500)
 
 def get():
-    results = run_statement('CALL get_all_appointments()')
+    is_valid_header = check_endpoint_info(request.headers, ['token'])
+    if(is_valid_header != None):
+        return make_response(json.dumps(is_valid_header, default=str), 400)
 
-    if(type(results) == list):
-        return make_response(json.dumps(results, default=str), 200)
+    valid_token = run_statement('CALL token_check(?)', [request.headers.get('token')])
+
+    if(type(valid_token) == list and len(valid_token) == 1):
+        results = run_statement('CALL get_all_appointments()')
+
+        if(type(results) == list):
+            return make_response(json.dumps(results, default=str), 200)
+        else:
+            return make_response(json.dumps("Sorry, an error has occurred", default=str), 500)
     else:
-        return make_response(json.dumps("Sorry, an error has occurred", default=str), 500)
+        return make_response(json.dumps("Wrong token", default=str), 400)
 
 def patch():
-    #is_valid_header = check_endpoint_info(request.headers, ['token'])
-    #if(is_valid_header != None):
-    #   return make_response(json.dumps(is_valid_header, default=str), 400)
+    is_valid_header = check_endpoint_info(request.headers, ['token'])
+    if(is_valid_header != None):
+        return make_response(json.dumps(is_valid_header, default=str), 400)
 
-    is_valid = check_endpoint_info(request.json, ['appointment_id'])
-    if(is_valid != None):
-        return make_response(json.dumps(is_valid, default=str), 400)
-    
-    appointment_info = run_statement('CALL get_appointment_by_user_token(?)', [request.json.get('appointment_id')])
+    valid_token = run_statement('CALL token_check(?)', [request.headers.get('token')])
 
-    update_appointment_info = check_data_sent(request.json, appointment_info[0], ['user_id', 'first_name', 'last_name', 'email', 'contract_signed', 'appointment_date'])
+    if(type(valid_token) == list and len(valid_token) == 1):
+        is_valid = check_endpoint_info(request.json, ['appointment_id'])
+        if(is_valid != None):
+            return make_response(json.dumps(is_valid, default=str), 400)
+        
+        appointment_info = run_statement('CALL get_appointment_by_id(?)', [request.json.get('appointment_id')])
 
-    results = run_statement('CALL edit_appointment(?,?,?,?,?,?,?)', [update_appointment_info['user_id'], update_appointment_info['first_name'], update_appointment_info['last_name'], update_appointment_info['email'], update_appointment_info['contract_signed'], update_appointment_info['appointment_date']])
+        update_appointment_info = check_data_sent(request.json, appointment_info[0], ['client_id', 'first_name', 'last_name', 'email', 'contract_signed', 'appointment_date'])
+
+        results = run_statement('CALL edit_appointment(?,?,?,?,?,?,?)', [update_appointment_info['client_id'], update_appointment_info['first_name'], update_appointment_info['last_name'], update_appointment_info['email'], update_appointment_info['contract_signed'], update_appointment_info['appointment_date'], request.json.get('appointment_id')])
+
+        if(type(results) == list and results[0]['row_updated'] == 1):
+            return make_response(json.dumps(results[0], default=str), 200)
+        elif(type(results) == list and results[0]['row_updated'] == 0):
+            return make_response(json.dumps(results[0], default=str), 400)
+        else:
+            return make_response(json.dumps("Sorry, an error has ocurred", default=str), 500)
+
+    else:
+        make_response(json.dumps("Wrong token.", default=str), 400)
 
