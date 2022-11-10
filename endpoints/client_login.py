172f8b7b20a239d307580a972f361c89631ecde0
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-11-10 16:12:41 -0700

    updating the client_login.py to have the token time validation

--- endpoints/client_login.py
+++ endpoints/client_login.py
@@ -1,5 +1,5 @@
 from flask import request, make_response
-from apihelpers import check_endpoint_info
+from apihelpers import check_endpoint_info,token_validation
 import secrets
 import json
 from dbhelpers import run_statement
@@ -24,11 +24,19 @@ def delete():
     if(is_valid_header != None):
         return make_response(json.dumps(is_valid_header, default=str), 400)
 
-    results = run_statement('CALL delete_client_token(?)', [request.headers.get('token')])
+    valid_token = token_validation(request.headers.get('token'))
+    if(valid_token == "valid"):    
+        results = run_statement('CALL delete_client_token(?)', [request.headers.get('token')])
 
-    if(type(results) == list and results[0]["row_updated"] == 1):
-        return make_response(json.dumps(results[0], default=str), 200)
-    elif(type(results) == list and results[0]["row_updated"] == 0):
-        return make_response(json.dumps("Wrong token.", default=str), 400)
+        if(type(results) == list and results[0]["row_updated"] == 1):
+            return make_response(json.dumps(results[0], default=str), 200)
+        elif(type(results) == list and results[0]["row_updated"] == 0):
+            return make_response(json.dumps("Wrong token.", default=str), 400)
+        else:
+            return make_response(json.dumps("Sorry, an error has occurred", default=str), 500)
+    elif(valid_token == "invalid"):
+        return make_response(json.dumps("TOKEN EXPIRED", default=str), 403)
+    elif(len(valid_token) == 0):
+        return make_response(json.dumps("WRONG TOKEN", default=str), 400)
     else:
-        return make_response(json.dumps("Sorry, an error has occurred", default=str), 500)
+        return make_response(json.dumps(valid_token, default=str), 500)
