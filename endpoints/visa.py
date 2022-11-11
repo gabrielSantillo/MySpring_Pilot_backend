511fe9f4e02ca41203e6f8ae2038edf2e23132c3
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-11-10 19:44:29 -0700

    updating the visa endpoint to have the token logic

--- endpoints/visa.py
+++ endpoints/visa.py
@@ -1,5 +1,5 @@
 from flask import request, make_response
-from apihelpers import check_endpoint_info, is_valid_token, check_data_sent
+from apihelpers import check_endpoint_info, is_valid_token, check_data_sent, token_validation
 import json
 from dbhelpers import run_statement
 
@@ -8,9 +8,8 @@ def post():
     if(is_valid_header != None):
         return make_response(json.dumps(is_valid_header, default=str), 400)
 
-    valid_token = is_valid_token(request.headers.get('token'))
-
-    if(valid_token):
+    valid_token = token_validation(request.headers.get('token'))
+    if(valid_token == "valid"):
         is_valid = check_endpoint_info(request.json, ['student_id', 'applied', 'applied_at', 'approved', 'analyst'])
         if(is_valid != None):
             return make_response(json.dumps(is_valid, default=str), 400)
@@ -18,23 +17,26 @@ def post():
         results = run_statement('CALL add_visa(?,?,?,?,?)', [request.json.get('student_id'), request.json.get('applied'), request.json.get('applied_at'), request.json.get('approved'), request.json.get('analyst')])
 
         if(type(results) == list and len(results) != 0):
-            return make_response(json.dumps(results, default=str), 200)
+            return make_response(json.dumps(results[0], default=str), 200)
         elif(type(results) == list and len(results) == 0):
             return make_response(json.dumps(results[0], default=str), 400)
         else:
             return make_response(json.dumps("Sorry, an error has occurred", default=str), 500)
 
+    elif(valid_token == "invalid"):
+        return make_response(json.dumps("TOKEN EXPIRED", default=str), 403)
+    elif(len(valid_token) == 0):
+        return make_response(json.dumps("WRONG TOKEN", default=str), 400)
     else:
-        return make_response(json.dumps("Wrong token", default=str), 400)
+        return make_response(json.dumps(valid_token, default=str), 500)
 
 def get():
     is_valid_header = check_endpoint_info(request.headers, ['token'])
     if(is_valid_header != None):
         return make_response(json.dumps(is_valid_header, default=str), 400)
 
-    valid_token = is_valid_token(request.headers.get('token'))
-
-    if(valid_token):
+    valid_token = token_validation(request.headers.get('token'))
+    if(valid_token == "valid"):
         results = run_statement('CALL get_all_visa()')
 
         if(type(results) == list and len(results) != 0):
@@ -43,17 +45,20 @@ def get():
             return make_response(json.dumps(results, default=str), 400)
         else:
             return make_response(json.dumps("Sorry, an error has occurred", default=str), 500)
+    elif(valid_token == "invalid"):
+        return make_response(json.dumps("TOKEN EXPIRED", default=str), 403)
+    elif(len(valid_token) == 0):
+        return make_response(json.dumps("WRONG TOKEN", default=str), 400)
     else:
-        return make_response(json.dumps("Wrong token", default=str), 400)
+        return make_response(json.dumps(valid_token, default=str), 500)
 
 def patch():
     is_valid_header = check_endpoint_info(request.headers, ['token'])
     if(is_valid_header != None):
         return make_response(json.dumps(is_valid_header, default=str), 400)
 
-    valid_token = is_valid_token(request.headers.get('token'))
-
-    if(valid_token):
+    valid_token = token_validation(request.headers.get('token'))
+    if(valid_token == "valid"):
         is_valid = check_endpoint_info(request.json, ['visa_id'])
         if(is_valid != None):
             return make_response(json.dumps(is_valid, default=str), 400)
@@ -75,5 +80,9 @@ def patch():
         else:
             return make_response(json.dumps("Wrong visa_id", default=str), 400)
 
+    elif(valid_token == "invalid"):
+        return make_response(json.dumps("TOKEN EXPIRED", default=str), 403)
+    elif(len(valid_token) == 0):
+        return make_response(json.dumps("WRONG TOKEN", default=str), 400)
     else:
-        return make_response(json.dumps("Wrong token", default=str), 400)
+        return make_response(json.dumps(valid_token, default=str), 500)
